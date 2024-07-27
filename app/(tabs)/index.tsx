96da1b2a856ac7f40bd1aaee0d4f45ce568e348c
Hiro Nguyen <78646685+imminist@users.noreply.github.com>
2024-07-27 13:33:45 +0900

    add ui

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,47 @@
-import { Text, View } from "react-native";
+import { Text, View, TextInput } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { FontAwesome6 } from "@expo/vector-icons";
 
 export default function HomeScreen() {
   return (
-    <View className=" flex flex-1 justify-center items-center">
-      <Text className="text-2xl">Hello World</Text>
-    </View>
+    <>
+      {/* Language selector row  */}
+
+      <View className="flex-row justify-around p-5">
+        <Text className="font-semibold color-blue-600">English</Text>
+        <FontAwesome5 name="exchange-alt" size={16} color="black" />
+        <Text className="font-semibold color-blue-600">Japanese</Text>
+      </View>
+      {/* Input Container  */}
+      <View className="border-y border-gray-300 p-5">
+        <View className="flex-row gap-5">
+          {/* Input */}
+          <TextInput
+            placeholder="Enter your text"
+            className="min-h-32 text-xl   flex-1"
+            multiline
+            maxLength={300}
+          />
+
+          {/* Send Button */}
+          <FontAwesome6 name="circle-arrow-right" size={24} color="royalblue" />
+        </View>
+        <View className="flex-row justify-between">
+          {/* Mic Icon */}
+          <FontAwesome6 name="microphone" size={18} color="dimgray" />
+          <Text className="color-gray-500">0 / 5000</Text>
+        </View>
+      </View>
+
+      {/* Output Container */}
+
+      <View className="gap-5 p-5 bg-gray-300">
+        <Text className="min-h-32 text-xl">Output</Text>
+        <View className="flex-row justify-between">
+          <FontAwesome6 name="volume-high" size={18} color="dimgray" />
+          <FontAwesome5 name="copy" size={18} color="dimgray" />
+        </View>
+      </View>
+    </>
   );
 }
