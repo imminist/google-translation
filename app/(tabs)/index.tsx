372b11d8063fb5a64112d0b2eb317143e6d06e84
Hiro Nguyen <78646685+imminist@users.noreply.github.com>
2024-08-05 11:18:54 +0900

    speech to text

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,24 +1,26 @@
 import { Text, View, TextInput } from 'react-native';
 import { FontAwesome5 } from '@expo/vector-icons';
+import Entypo from '@expo/vector-icons/Entypo';
 import { FontAwesome6 } from '@expo/vector-icons';
 import { Audio } from 'expo-av';
 
 import { useState } from 'react';
 import { supabase } from '@/utils/supabase';
+import * as FileSystem from 'expo-file-system';
 
 export default function HomeScreen() {
   const [input, setInput] = useState('');
   const [output, setOutput] = useState('');
 
+  const [recording, setRecording] = useState<Audio.Recording>();
+  const [permissionResponse, requestPermission] = Audio.usePermissions();
+
   const translate = async (text: string) => {
-    const { data, error } = await supabase.functions.invoke('translate', {
-      body: JSON.stringify({ input, from: 'English', to: 'Japanese' }),
+    const { data } = await supabase.functions.invoke('translate', {
+      body: JSON.stringify({ input: text, from: 'English', to: 'Japanese' }),
     });
 
-    console.log(error);
-    console.log('Hello', data);
-
-    return data?.content || 'Something went wrong';
+    return data?.content || 'Something went wrong!';
   };
 
   const onTranslate = async () => {
@@ -27,12 +29,9 @@ export default function HomeScreen() {
   };
 
   const textToSpeech = async (text: string) => {
-    const { data, error } = await supabase.functions.invoke('text-to-speech', {
+    const { data } = await supabase.functions.invoke('text-to-speech', {
       body: JSON.stringify({ input: text }),
     });
-
-    console.log(data);
-    console.log(error);
 
     if (data) {
       const { sound } = await Audio.Sound.createAsync({
@@ -41,6 +40,53 @@ export default function HomeScreen() {
       sound.playAsync();
     }
   };
+
+  async function startRecording() {
+    try {
+      if (permissionResponse?.status !== 'granted') {
+        console.log('Requesting permission..');
+        await requestPermission();
+      }
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+
+      console.log('Starting recording..');
+      const { recording } = await Audio.Recording.createAsync(Audio.RecordingOptionsPresets.HIGH_QUALITY);
+      setRecording(recording);
+      console.log('Recording started');
+    } catch (err) {
+      console.error('Failed to start recording', err);
+    }
+  }
+
+  async function stopRecording() {
+    if (!recording) {
+      return;
+    }
+    console.log('Stopping recording..');
+    setRecording(undefined);
+    await recording.stopAndUnloadAsync();
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+    });
+    const uri = recording.getURI();
+    console.log('Recording stopped and stored at', uri);
+
+    if (uri) {
+      const audioBase64 = await FileSystem.readAsStringAsync(uri, {
+        encoding: 'base64',
+      });
+      const { data, error } = await supabase.functions.invoke('speech-to-text', {
+        body: JSON.stringify({ audioBase64 }),
+      });
+      setInput(data.text);
+      const translation = await translate(data.text);
+      setOutput(translation);
+
+    }
+  }
 
   return (
     <View>
@@ -65,16 +111,17 @@ export default function HomeScreen() {
           />
 
           {/* Send Button */}
-          <FontAwesome6
-            onPress={onTranslate}
-            name='circle-arrow-right'
-            size={24}
-            color='royalblue'
-          />
+          <FontAwesome6 onPress={onTranslate} name='circle-arrow-right' size={24} color='royalblue' />
         </View>
         <View className='flex-row justify-between'>
           {/* Mic Icon */}
-          <FontAwesome6 name='microphone' size={18} color='dimgray' />
+
+          {recording ? (
+            <Entypo onPress={stopRecording} name='controller-stop' size={24} color='royalblue' />
+          ) : (
+            <FontAwesome6 onPress={startRecording} name='microphone' size={18} color='dimgray' />
+          )}
+
           <Text className='color-gray-500'>{input.length} / 5000</Text>
         </View>
       </View>
@@ -85,12 +132,7 @@ export default function HomeScreen() {
         <View className='gap-5 p-5 bg-gray-200'>
           <Text className='min-h-32 text-xl'>{output}</Text>
           <View className='flex-row justify-between'>
-            <FontAwesome6
-              name='volume-high'
-              size={18}
-              color='dimgray'
-              onPress={() => textToSpeech(output)}
-            />
+            <FontAwesome6 name='volume-high' size={18} color='dimgray' onPress={() => textToSpeech(output)} />
             <FontAwesome5 name='copy' size={18} color='dimgray' />
           </View>
         </View>
